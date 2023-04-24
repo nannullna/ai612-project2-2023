5c87d74025a1ea20e32346d9b5f1369bab00bf13
ojoo-J <ojooipad@gmail.com>
2023-04-24 15:12:24 +0900

    yj_train

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python train.py \
 --student_number 00000000 \
---data_path /root/healthcare/ai612-project2-2023/ \
+--data_path /data5/youngju/ai612-project2-2023/outcome/ \
 --model_path emilyalsentzer/Bio_ClinicalBERT \
 --batch_size 2 \
 --valid_percent 0.2 \
---log_interval 5000 \
+--log_interval 50 \
 # --bert_unfreeze
---device_id 1
+--device_id 0
