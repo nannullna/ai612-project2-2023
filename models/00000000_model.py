3f81b1c76a537e7e971e49583727e5f52aca0195
ojoo-J <ojooipad@gmail.com>
2023-04-26 15:18:16 +0900

    implement transformer encoder

--- models/00000000_model.py
+++ models/00000000_model.py
@@ -40,19 +40,27 @@ class MyModel00000000(BaseModel):
                 if i < 8:
                     for param in module.parameters():
                         param.requires_grad = False
-
-
+                        
+                        
+        self.embedding_dim = 768
+        self.transformer_dim = 512
+        
+        
+        # intermediate linear btw embedding model and Aggregator
+        self.Inter_Linear = nn.Linear(self.embedding_dim, self.transformer_dim)
+        
+        
         # Event Aggregator
-        self.agg = RNNModel()
+        self.agg = Transformer()
 
         # Custom layers
-        custom_hidden_dim = 64
+        self.custom_hidden_dim = 128
         self.customs = nn.Sequential(
-            nn.Dropout(0.3),
-            nn.Linear(self.agg.final_dim, custom_hidden_dim), # (128, 64)
+            nn.Dropout(0.2),
+            nn.Linear(self.agg.final_dim, self.custom_hidden_dim), # (256, 128)
             nn.ReLU(),
-            nn.Dropout(0.25),
-            nn.Linear(custom_hidden_dim, 52)
+            nn.Dropout(0.2),
+            nn.Linear(self.custom_hidden_dim, 52)
         )
 
     def get_logits(cls, net_output):
@@ -99,75 +107,53 @@ class MyModel00000000(BaseModel):
                 def forward(self, data_key, **kwargs):
                     (...)
         """
-        embeddings = [self.bert(input_ids=i, attention_mask=m)[1] for i, m in zip(input_ids, attention_mask)]
-
-        # _, embedding = self.bert(
-        #     input_ids=input_ids.squeeze(0),
-        #     attention_mask=attention_mask.squeeze(0) # 원래 기대하는 shape는 (bs, max sequence length)
-        # ) # output > (ts, 768)
-
-        embeddings = torch.stack(embeddings) # (bs, max_timestep, 768)
-
-        agg = self.agg(embeddings, kwargs['timesteps'])  # output > (2, 128)
-        output = self.customs(agg) # (2, 52)
+        embeddings = [torch.mean(self.bert(input_ids=i, attention_mask=m)[0], dim=1) for i, m in zip(input_ids, attention_mask)] # average-pooling
+        embeddings = self.Inter_Linear(torch.stack(embeddings)) # (bs, max_timestep, 768) -> (bs, max_timestep, 512)
+        
+        agg = self.agg(embeddings)  # (bs, max_timestep, 512) -> (bs, 256)
+        output = self.customs(agg) # (bs, 52)
 
         return output
 
-# RNN for Aggregation    
-class RNNModel(nn.Module):
-    def __init__(self, **kwargs):
+# Transformer for Aggregation    
+class Transformer(nn.Module):
+    def __init__(self):
         super().__init__()
 
-        self.batch_size = 1
-        input_size = 768
-        self.hidden_dim = 256
-        self.final_dim = 128
-        drop_out = 0.3
-        self.n_layers = 1
-
-        self.model = nn.GRU(
-            input_size,
-            self.hidden_dim,
-            dropout=drop_out,
-            batch_first=True,
-            bidirectional=False,
-            num_layers=self.n_layers
+        self.dim_model = 512
+        self.num_heads = 8
+        self.num_encoder_layers = 6
+        self.dropout_p = 0.2
+        self.final_dim = 256
+        
+        self.encoder_layer = nn.TransformerEncoderLayer(self.dim_model, self.num_heads)
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer = self.encoder_layer,
+            num_layers = self.num_encoder_layers
         )
-
-        self.dropout = nn.Dropout(0.3)
 
         self.final_proj = nn.Sequential(
             nn.Linear(
-                self.hidden_dim,
+                self.dim_model,
                 self.final_dim
-            ),
-            nn.ReLU()
+            )
         )
 
-    def forward(self, x, seq_len, **kwargs):
-        # self.model.flatten_parameters()
-
-        output_seq, _ = self.pack_pad_seq(x, seq_len)
-        output_seq = output_seq[:, -1] # (2, 256)
-        # h_0 = self._init_state(batch_size=x.size(0))
-        # x, _ = self.model(x, h_0)
-        # h_t = x[:, -1]
-
-        self.dropout(output_seq)
-        output = self.final_proj(output_seq)
-        
+    def forward(self, x):
+        output = torch.mean(self.encoder(x),dim=1) # (bs, max_timestep, 512) -> (bs, 512)
+        output = self.final_proj(output) # (bs, 512) -> (bs, 256)
         return output
     
-    def _init_state(self, batch_size=1):
-        weight = next(self.parameters()).data
-        weight = weight.new(self.n_layers, batch_size, self.hidden_dim).zero_()
-        return weight
+    # def _init_state(self, batch_size=1):
+    #     weight = next(self.parameters()).data
+    #     weight = weight.new(self.n_layers, batch_size, self.hidden_dim).zero_()
+    #     return weight
 
-    def pack_pad_seq(self, x, lengths):
-        lengths = torch.tensor(lengths).cpu()
+    # def pack_pad_seq(self, x, lengths):
+    #     lengths = torch.tensor(lengths).cpu()
 
-        packed =  pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
-        output, _ = self.model(packed)
-        output_seq, output_len = pad_packed_sequence(output, batch_first=True, padding_value=0)
+    #     packed =  pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+    #     output, _ = self.model(packed)
+    #     output_seq, output_len = pad_packed_sequence(output, batch_first=True, padding_value=0)
         
-        return output_seq, output_len
+    #     return output_seq, output_len
